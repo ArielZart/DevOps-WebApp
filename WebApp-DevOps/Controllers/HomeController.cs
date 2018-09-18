1112774e61309c71a9e0000634ffa89ae4489b1c
ArielZart <ariel.hz@hotmail.com>
2018-09-18 14:50:20 -0300

    Update HomeController.cs

--- WebApp-DevOps/Controllers/HomeController.cs
+++ WebApp-DevOps/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApp_DevOps.Controllers
     {
         public ActionResult Index()
         {
-            return Content("Hello World, teste Odílon !!!!");
+            return Content("Hello World, teste Cappello !!!!");
         } 
         
         public ActionResult About()
